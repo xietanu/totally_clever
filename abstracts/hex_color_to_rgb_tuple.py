9cf163bba78a97f70bbe0fa3d3b526568fef4255
xietanu <sutton.matt.p@gmail.com>
2022-03-12 21:17:33 +0000

    Add conversion between hex colour and rgb tuple

---
+++ abstracts/hex_color_to_rgb_tuple.py
@@ -0,0 +1,7 @@
+"""hex_colour_to_rgb_tuple"""
+
+
+def hex_colour_to_rgb_tuple(hex_colour: str) -> tuple[int, int, int]:
+    """Takes a hex values (e.g. #FF8000) and returns an RGB tuple (e.g. (255,128,0))"""
+    hex_colour = hex_colour[-6:]
+    return (int(hex_colour[i : i + 1], 16) for i in range(0, 6, 2))
