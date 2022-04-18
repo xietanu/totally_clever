938197f99992d1874f15cfa5886f604e32b6e452
xietanu <sutton.matt.p@gmail.com>
2022-04-18 22:53:10 +0100

    Ran black

--- states/selecting_die.py
+++ states/selecting_die.py
@@ -2,7 +2,9 @@
 from states import game_state
 
 
-class SelectingDieGameState(game_state.GameState): # pylint: disable=too-few-public-methods
+class SelectingDieGameState(
+    game_state.GameState
+):  # pylint: disable=too-few-public-methods
     """State for the selecting and assigning dice"""
 
     def end(self):
